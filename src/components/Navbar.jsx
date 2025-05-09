7ea8229ef8b324fadd5e27df9d6e2bf3a8820052
pierreprime2 <96549722+pierreprime2@users.noreply.github.com>
2025-05-09 13:52:26 +0200

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+
+function Navbar() {
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                    Pokémon Arena
+                </Typography>
+                <Button color="inherit">Home</Button>
+                <Button color="inherit">Fight</Button>
+                <Button color="inherit">Statistics</Button>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h2>Welcome to Pokémon arena</h2>
+            <p>Choose your Pokémon team and prepare for battle!</p>
+        </div>
+    )
+}
+
+export default Home
